53809745288102dd820d6acc3ec403e14daa2604
deepakgouda <deepakgouda1729@gmail.com>
2019-04-27 10:47:01 +0000

    Added run_script.sh

---
+++ run_script.sh
@@ -0,0 +1,6 @@
+./waf --run "scratch/script TcpNewReno"
+./waf --run "scratch/script TcpHybla"
+./waf --run "scratch/script TcpWestwood"
+./waf --run "scratch/script TcpScalable"
+./waf --run "scratch/script TcpVegas"
+
